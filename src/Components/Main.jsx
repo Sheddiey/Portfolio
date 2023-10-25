5b9794b8b8f62f61e842112d67339375d4694b62
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-10-25 11:56:19 +0300

    completed Home component

---
+++ src/Components/Main.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXTwitter,
+  faLinkedinIn,
+  faWhatsapp,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+export function Main() {}
+
+export function Home() {
+  return (
+    <div
+      className="card grid md:place-content-evenly justify-items-center justify-center content-center md:flex"
+      id="main"
+    >
+      <section className="order-last md:grid md:content-center md:-order-1">
+        <Socials />
+      </section>
+      <section className="intro-description">
+        <h2>
+          Hello, I am <span>Shadrack Cheruiyot</span>{" "}
+        </h2>
+        <h5>
+          I am a passionate front-end developer with a keen eye for design and a
+          love for creating user-friendly, visually appealing web experiences. I
+          enjoy turning ideas into reality through code and bringing responsive
+          designs to life.
+        </h5>
+        <button className="button">
+          <a
+            target="blank"
+            href="https://api.whatsapp.com/send?phone=254711241878"
+          >
+            Hire me
+          </a>
+        </button>
+      </section>
+    </div>
+  );
+}
+
+function Socials() {
+  return (
+    <ul className="gap-10 md:grid flex list-none justify-center">
+      <li>
+        <a target="blank" href='https://twitter.com/sheddieynext"'>
+          <FontAwesomeIcon className="icon" icon={faXTwitter} />
+        </a>
+      </li>
+      <li>
+        <a target="blank" href="https://www.linkedin.com/in/sheddiey/">
+          <FontAwesomeIcon className="icon" icon={faLinkedinIn} />
+        </a>
+      </li>
+      <li>
+        <a
+          target="blank"
+          href="https://api.whatsapp.com/send?phone=254711241878"
+        >
+          <FontAwesomeIcon className="icon" icon={faWhatsapp} />
+        </a>
+      </li>
+      <li>
+        <a target="blank" href="https://github.com/Sheddiey">
+          <FontAwesomeIcon className="icon" icon={faGithub} />
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export function About() {}
+
+export function Skills() {}
+
+export function Services() {}
+
+export function Projects() {}
+
+export function ContactMe() {}
