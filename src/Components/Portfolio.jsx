1f635e87bd168e4142c888f2602e533d586aaa88
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-11-04 12:51:31 +0300

    added a toggle button

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { About, ContactMe, Home, Projects, Skills } from "./Main";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import sheddiey from "./sheddie ed.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faDownload, faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
 export function Portfolio() {
   return (
     <Router>
       <div className=" h-screen flex flex-col justify-between ">
         <NavBar />
-        <div className="flex">
+        <div className="md:flex grid gap-10">
           <Routes>
             <Route path="/" Component={Home} />
             <Route path="/about" Component={About} />
@@ -27,36 +33,64 @@ export function Portfolio() {
 }
 
 function NavBar() {
+  const [isToggle, setIsToggle] = useState(false);
+  const location = useLocation();
+
   return (
-    <nav className=" flex h-15 regal-blue box-shadow p-3 justify-between">
-      <h2 className="cursor-pointer transition ease-in-out delay-150 hover:underline-offset-1">
+    <nav className=" flex h-12 regal-blue box-shadow p-3 justify-between">
+      <h2 className="cursor-pointer font-bold hover:text-2xl hover:transition hover:delay-150 duration-300 ease-in-out">
         <Link to="/">Sheddiey</Link>
       </h2>
-      <ul className="flex cursor-pointer gap-5">
+      <ul className="md:flex hidden cursor-pointer md:gap-5">
         <li>
-          <Link className='hover:text-sky-600' to="/">Home</Link>
+          <Link className="hover:text-sky-600" to="/">
+            Home
+          </Link>
         </li>
         <li>
-          <Link className='hover:text-sky-600' to="/about">About</Link>
+          <Link className="hover:text-sky-600" to="/about">
+            About
+          </Link>
         </li>
         <li>
-          <Link className='hover:text-sky-600' to="/skills">Skills</Link>
+          <Link className="hover:text-sky-600" to="/skills">
+            Skills
+          </Link>
         </li>
         <li>
-          <Link className='hover:text-sky-600' to="/projects">Projects</Link>
+          <Link className="hover:text-sky-600" to="/projects">
+            Projects
+          </Link>
         </li>
         <li>
-          <Link className='hover:text-sky-600' to="/contactMe">Contact Me</Link>
+          <Link className="hover:text-sky-600" to="/contactMe">
+            Contact Me
+          </Link>
         </li>
       </ul>
+      <div className="md:hidden">
+        <ToggleButton isToggle={isToggle} />
+      </div>
     </nav>
+  );
+}
+
+function ToggleButton({ isToggle }) {
+  return (
+    <button>
+      {isToggle ? (
+        <FontAwesomeIcon icon={faBars} />
+      ) : (
+        <FontAwesomeIcon icon={faX} />
+      )}
+    </button>
   );
 }
 
 function Footer() {
   return (
     <div className="w-screen h-10">
-      <p className="text-center cursor-pointer transition- opacity-75 hover:opacity-100 transition-opacity">
+      <p className="text-center cursor-pointer transition- opacity-50 hover:opacity-100 transition-opacity">
         © Sheddiey 2023. All rights reserved.
       </p>
     </div>
@@ -66,8 +100,17 @@ function Footer() {
 function Avatar() {
   return (
     <div className="flex flex-col gap-10">
-      <img className="h-80 hover:shadow-[0-3px-8px-0_ hsl(192, 100%, 9%)] cursor-pointer w-80" src={sheddiey} alt="sheddiey" />
-      <FontAwesomeIcon className="icon hover:shadow-2xl cursor-pointer hover:text-sky-600 text-2xl" icon={faDownload} />
+      <img
+        className="h-80 hover:shadow-[0-3px-8px-0_ hsl(192, 100%, 9%)] cursor-pointer w-80"
+        src={sheddiey}
+        alt="sheddiey"
+      />
+      <div className="text-center">
+        <FontAwesomeIcon
+          className="icon text-2xl hover:text-sky-600 cursor-pointer"
+          icon={faDownload}
+        />
+      </div>
     </div>
   );
 }
