675a5757f61cd5f4d384147898f8a2771bf94477
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2023-11-08 10:18:08 +0300

    fixed the footer

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -103,7 +103,7 @@ function ToggleButton({ isToggle, handleClick }) {
 
 function Footer() {
   return (
-    <div className="w-screen absolute md:relative bottom-0 h-10">
+    <div className="w-screen  md:relative bottom-0 h-10">
       <p className="text-center cursor-pointer transition- opacity-50 hover:opacity-100 transition-opacity">
         © Sheddiey 2023. All rights reserved.
       </p>
