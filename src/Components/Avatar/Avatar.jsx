1409188af2019795017d305cccdefddb34da8af3
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2024-02-02 17:01:35 +0300

    added a popper for the cv download button

--- src/Components/Avatar/Avatar.jsx
+++ src/Components/Avatar/Avatar.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import React from "react";
 import sheddiey from "../Assets/sheddie ed.jpg";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip } from "react-tippy";
+import 'react-tippy/dist/tippy.css';
 
 const Avatar = () => {
   return (
     <div className="flex items-center flex-col gap-5 md:gap-10">
-    <img
-      className="lg:h-80 w-[85%] lg:mt-[20px] h-[60%] lg:w-80 h-60 w-60 hover:shadow-[0-3px-8px-0_ hsl(192, 100%, 9%)] cursor-pointer w-80"
-      src={sheddiey}
-      alt="sheddiey"
-    />
-    <div className="text-center">
-      <FontAwesomeIcon
-        className="icon text-2xl hover:text-sky-600 cursor-pointer"
-        icon={faDownload}
+      <img
+        className="lg:h-80 w-[85%] lg:mt-[20px] h-[60%] lg:w-80 h-60 w-60 hover:shadow-[0-3px-8px-0_ hsl(192, 100%, 9%)] cursor-pointer w-80"
+        src={sheddiey}
+        alt="sheddiey"
       />
+      <div className="text-center">
+        <Tooltip
+          title="Download CV"
+          position="bottom"
+          trigger="mouseenter"
+          arrow={true}
+          interactive={true}
+        >
+          <FontAwesomeIcon
+            className="icon text-2xl hover:text-sky-600 cursor-pointer"
+            icon={faDownload}
+          />
+        </Tooltip>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,8 +6,8 @@ import oningoiDecals from "../Assets/3-a.png";
 import restApi from "../Assets/2-c.png";
 import fintrack_e from "../Assets/1-f.png";
 import fintrackPhone from "../Assets/fintrackPhone.png";
-import oningoiDecalsPhone from "../Assets/oningoiDecalsPhone.png"
-import restApiPhone from "../Assets/restApiPhone.png"
+import oningoiDecalsPhone from "../Assets/oningoiDecalsPhone.png";
+import restApiPhone from "../Assets/restApiPhone.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -36,18 +36,20 @@ const Projects = ({ projectsClass }) => {
     autoplaySpeed: 4000,
   };
 
-  
-
   return (
     <Slider className={projectsClass} {...settings}>
       <div>
         <h3 className="font-semibold text-center mb-[5px]">FinTrack</h3>
         {isSmallScreen ? (
-          <img className="h-[320px] w-[80%] mx-[auto]" src={fintrackPhone} alt="" />
+          <img
+            className="h-[320px] w-[80%] mx-[auto]"
+            src={fintrackPhone}
+            alt=""
+          />
         ) : (
           <img className="h-[280px] mx-[auto]" src={fintrack_e} alt="" />
         )}
-        <p className="text-[10px]">
+        <p className="text-[10px] mt-[5px]">
           The monthly tracker web app uses React with Tailwind CSS for frontend
           design and Firebase for backend services, enabling user
           authentication, data storage, and management.
@@ -69,14 +71,24 @@ const Projects = ({ projectsClass }) => {
         </div>
       </div>
       <div>
-        <h3 className="font-semibold text-center mb-[5px]">Oningoi E-commerce Platform</h3>
+        <h3 className="font-semibold text-center mb-[5px]">
+          Oningoi E-commerce Platform
+        </h3>
         {isSmallScreen ? (
-          <img className="h-[320px] w-[80%] mx-[auto]" src={oningoiDecalsPhone} alt="" />
+          <img
+            className="h-[320px] w-[80%] mx-[auto]"
+            src={oningoiDecalsPhone}
+            alt=""
+          />
         ) : (
           <img className="h-[280px] mx-[auto]" src={oningoiDecals} alt="" />
         )}
-        <p></p>
-        <div className="flex mt-[30px] justify-between duration-300 px-[100px] md:px-[250px] text-[20px]">
+        <p className="text-[10px] mt-[5px]">
+          Sticker Shop, an e-commerce site, utilizes React and CSS for an
+          engaging sticker shopping experience, featuring easy cart management
+          and real-time cost updates for users to explore and enjoy.
+        </p>
+        <div className="flex justify-between duration-300 px-[100px] md:px-[250px] text-[20px]">
           <a href="https://oningoi-e-commerce.web.app/" target="blank">
             <FontAwesomeIcon
               className="cursor-pointer hover:text-[#0284c7]  transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110  duration-300"
@@ -95,14 +107,24 @@ const Projects = ({ projectsClass }) => {
         </div>
       </div>
       <div>
-        <h3 className="font-semibold text-center  mb-[5px]">Rest Countries API </h3>
+        <h3 className="font-semibold text-center  mb-[5px]">
+          Rest Countries API{" "}
+        </h3>
         {isSmallScreen ? (
-          <img className="h-[320px] w-[80%] mx-[auto]" src={restApiPhone} alt="" />
+          <img
+            className="h-[320px] w-[80%] mx-[auto]"
+            src={restApiPhone}
+            alt=""
+          />
         ) : (
           <img className="h-[280px] mx-[auto]" src={restApi} alt="" />
         )}
-        <p></p>
-        <div className="flex mt-[30px] justify-between duration-300 px-[100px] md:px-[250px] text-[20px]">
+        <p className="text-[10px] mt-[5px]">
+          Discover countries worldwide with a React-powered frontend showcasing
+          filter by region and search functionalities, offering a seamless
+          exploration experience of the REST Countries API.
+        </p>
+        <div className="flex justify-between duration-300 px-[100px] md:px-[250px] text-[20px]">
           <a
             href="https://rest-countries-api-zeta-six.vercel.app/"
             target="blank"
@@ -128,4 +150,3 @@ const Projects = ({ projectsClass }) => {
 };
 
 export default Projects;
-
